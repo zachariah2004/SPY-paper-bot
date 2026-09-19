d7078c69dd22040ae9e6f68826989628c8073e9c
zachariah2004 <zachrichardson2004@gmail.com>
2026-09-20 00:19:13 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-import time; print(123); time.sleep(600)
+import os
+from alpaca.trading.client import TradingClient
+
+api_key = os.environ["APCA_API_KEY_ID"]
+secret_key = os.environ["APCA_API_SECRET_KEY"]
+
+client = TradingClient(
+    api_key=api_key,
+    secret_key=secret_key,
+    paper=True
+)
+
+account = client.get_account()
+
+print("PAPER CONNECTION SUCCESS")
+print("Account status:", account.status)
+print("Buying power:", account.buying_power)
